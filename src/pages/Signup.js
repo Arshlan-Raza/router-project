df7a654dd34a8d48951605caf8f985dcc89f8544
Arshlan Raza <arshalanraja@gmail.com>
2024-03-09 22:12:39 +0530

    finished with the component

---
+++ src/pages/Signup.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Template from '../components/Template';
+import signImage from '../assets/signup.png'
+
+
+const Signup = ({setIsLoggedIn}) => {
+  
+  return (
+    <Template
+      title="Welcome Back"
+      desc1="Build skills for today, tomorrow, and beyond."
+      desc2="Education to future-proof your career."
+      image={signImage}
+      formType="signup"
+      setIsLoggedIn={setIsLoggedIn}
+    />
+  )
+}
+
+export default Signup;
